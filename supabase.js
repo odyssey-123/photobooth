7d7c4862dc173b065c0803b6ee7b47d2c0be1482
odyssey-123 <info.odyssey12@gmail.com>
2026-06-23 14:18:36 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://gixxpfzefzzeooiwnodj.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_QgR6vohyKbfNDn7-xmdcwA_xfRjpKeW";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
