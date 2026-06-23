9cb3961962cdbc9aa84831247746351a3d649639
odyssey-123 <info.odyssey12@gmail.com>
2026-06-23 14:18:51 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,105 @@
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+
+const captureBtn = document.getElementById("captureBtn");
+const retakeBtn = document.getElementById("retakeBtn");
+const uploadBtn = document.getElementById("uploadBtn");
+
+const loading = document.getElementById("loading");
+const success = document.getElementById("success");
+
+const ctx = canvas.getContext("2d");
+
+let photoBlob = null;
+
+// Start Camera
+async function startCamera() {
+
+    try {
+
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: {
+                facingMode: "user"
+            },
+            audio: false
+        });
+
+        video.srcObject = stream;
+
+    } catch (err) {
+
+        alert("Could not access camera.");
+
+        console.error(err);
+
+    }
+
+}
+
+startCamera();
+
+// Capture Photo
+captureBtn.addEventListener("click", () => {
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    ctx.drawImage(video, 0, 0);
+
+    canvas.style.display = "block";
+    video.style.display = "none";
+
+    captureBtn.style.display = "none";
+    retakeBtn.style.display = "block";
+    uploadBtn.style.display = "block";
+
+});
+
+// Retake
+retakeBtn.addEventListener("click", () => {
+
+    canvas.style.display = "none";
+    video.style.display = "block";
+
+    captureBtn.style.display = "block";
+    retakeBtn.style.display = "none";
+    uploadBtn.style.display = "none";
+
+});
+
+// Upload
+uploadBtn.addEventListener("click", () => {
+
+    loading.style.display = "block";
+
+    canvas.toBlob(async function(blob){
+
+        photoBlob = blob;
+
+        const fileName = "photo_" + Date.now() + ".jpg";
+
+        const { error } = await supabaseClient
+            .storage
+            .from("Photos")
+            .upload(fileName, photoBlob);
+
+        loading.style.display = "none";
+
+        if(error){
+
+            alert(error.message);
+            console.log(error);
+
+            return;
+
+        }
+
+        success.style.display = "block";
+
+        captureBtn.style.display = "none";
+        retakeBtn.style.display = "none";
+        uploadBtn.style.display = "none";
+
+    }, "image/jpeg", 0.95);
+
+});
